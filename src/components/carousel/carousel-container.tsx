1a233d39ee7f0d7cf772e357278c385430ef5183
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-01-10 11:13:57 -0300

    update imports

--- src/components/carousel/carousel-container.tsx
+++ src/components/carousel/carousel-container.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { CarouselMobile } from "./carousel-mobile";
-import { CarouselLg } from "./carousel-lg";
-import { CarouselMd } from "./carousel-md";
+import { CarouselWeb } from "./carousel-web";
 
 export function CarouselContainer() {
   return (
@@ -13,17 +12,12 @@ export function CarouselContainer() {
       >
         <CarouselMobile />
       </div>
-      {/* <div
-        id="gallery"
-        className="hidden md:flex 1400:hidden w-full min-h-full items-center justify-center pb-[50px] py-40"
-      >
-        <CarouselMd />
-      </div> */}
+
       <div
         id="gallery"
         className="hidden md:flex w-full min-h-full items-center justify-center pb-[50px] py-40"
       >
-        <CarouselLg />
+        <CarouselWeb />
       </div>
     </>
   );
