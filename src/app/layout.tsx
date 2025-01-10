9696341dd84cee0f59710f5fd683b6dacaf923c9
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-01-10 11:02:40 -0300

    remove unused styles and imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { AboutUSContainer } from "@/components/about-us/about-us-container";
 import { Home } from "@/components/home/home";
-import { CarouselContainer } from "@/components/carousel/carousel-container";
-import { ContactContainer } from "@/components/contact/contact-container";
 import { WhereWeAreMap } from "@/components/where-we-are/map";
 import { GroupContainer } from "@/components/group/group-container";
+import { ContactContainer } from "@/components/contact/contact-container";
+import { AboutUSContainer } from "@/components/about-us/about-us-container";
+import { CarouselContainer } from "@/components/carousel/carousel-container";
 
 export default function App() {
   return (
